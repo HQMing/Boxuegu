38666116c051e5342ca65458402573cbe086a6b6
apple <1106665408@qq.com>
2018-04-10 17:28:49 +0800

    "个人资料"部分

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/Fragment/FragmentMyinfoFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/Fragment/FragmentMyinfoFragment.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import cn.edu.gdmec.android.boxuegu.R;
 import cn.edu.gdmec.android.boxuegu.activity.ActivitySettingActivity;
+import cn.edu.gdmec.android.boxuegu.activity.ActivityUserInfoActivity;
 import cn.edu.gdmec.android.boxuegu.activity.LoginActivity;
 import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
 
@@ -62,8 +63,8 @@ public class FragmentMyinfoFragment extends Fragment implements View.OnClickList
             case R.id.ll_head:
                 if (AnalysisUtils.readLoginStatus(getActivity())) {
                     //跳转到个人资料界面
-                    //Intent intent = new Intent(getActivity(),ActivityUserInfoActivity.class);
-                   // getActivity().startActivity(intent);
+                    Intent intent = new Intent(getActivity(),ActivityUserInfoActivity.class);
+                    getActivity().startActivity(intent);
                 } else {
                     //跳转到登陆界面
                     Intent intent = new Intent(getActivity(), LoginActivity.class);
